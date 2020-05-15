e9eebb9667d17b3801ecb1e20e5426e350987742
Amy Franz <amy.franz@gmail.com>
2020-05-15 20:17:46 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  console.log(state.friends.find((friend) => friend.id === action.id));
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.id),
+      };
+    default:
+      return state;
+  }
 }
